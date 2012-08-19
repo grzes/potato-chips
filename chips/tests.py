3d05f0a6862b758a5ffadf7eb0d393f92f43db43
Grzes Furga <grzesiof@gmail.com>
2012-08-20 00:33:36 +0200

    Figured out how to mock  a logged in user

--- chips/tests.py
+++ chips/tests.py
@@ -1,17 +1,43 @@
+import os
+from copy import copy
 from pypath import pypath;pypath()
 import unittest
+from django.test.client import Client
+from django.conf import settings
+
 from google.appengine.api import memcache
 from google.appengine.ext import db
 from google.appengine.ext import testbed
 
+from django.core.urlresolvers import reverse
+from chips.models import Blog
 
-class ATestSuit(unittest.TestCase):
 
+settings.SITE_DOMAIN = 'testserver' # our blog selection middleware needs to match on the host
+
+
+
+class UserBlog(unittest.TestCase):
     def setUp(self):
-        self.param = 1
+        self.testbed = testbed.Testbed()
+        self.testbed.activate()
+        self.testbed.init_datastore_v3_stub()
+        self.testbed.init_user_stub(enable=not False)
 
-    def test_param(self):
-        self.assertEqual(self.param, 1)
+    def login(self, email='', user_id=''):
+        default_env = copy(testbed.DEFAULT_ENVIRONMENT)
+        self.testbed.setup_env(overwrite=True, user_email=email, user_id=user_id)
+
+    def tearDown(self):
+        self.testbed.deactivate()
+
+    def test_a_blogless_user(self):
+        """Without a blog a user is redirected from his dash to the signup page"""
+        self.login('test@example.com', user_id='1')
+        client = Client()
+        response = client.get(reverse('dash'))
+        self.assertEqual('http://testserver'+reverse('signup'), response['location'])
+
 
 if __name__ == '__main__':
     unittest.main()
